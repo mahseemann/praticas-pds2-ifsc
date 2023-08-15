fa73918fb80d64b6f7d9c7d8457d06e8489da989
Maiara Junches Seemann <111303592+mahseemann@users.noreply.github.com>
2023-08-15 10:23:48 -0300

    Add files via upload

---
+++ projetomaven/src/main/java/controle/PessoaDAO.java
@@ -0,0 +1,12 @@
+package controle;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modelo.Pessoa;
+
+public class PessoaDAO {
+
+}
