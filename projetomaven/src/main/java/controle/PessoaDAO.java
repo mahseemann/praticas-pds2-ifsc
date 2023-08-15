70e5c561ee85d6651167e5d9221a830abf953254
Maiara Junches Seemann <111303592+mahseemann@users.noreply.github.com>
2023-08-15 10:38:23 -0300

    Add files via upload

--- projetomaven/src/main/java/controle/PessoaDAO.java
+++ projetomaven/src/main/java/controle/PessoaDAO.java
@@ -8,5 +8,13 @@ import java.util.ArrayList;
 import modelo.Pessoa;
 
 public class PessoaDAO {
+	
+	public boolean inserir(Pessoa p) {
+		
+		Conexao c = Conexao.getInstacia();
+		Connection con = c.conectar();
+		
+		return true;
+	}
 
 }
--- projetomaven/src/main/java/modelo/Pessoa.java
+++ projetomaven/src/main/java/modelo/Pessoa.java
@@ -1,5 +1,33 @@
 package modelo;
 
 public class Pessoa {
+	
+	private int idPessoa;
+	private String primeiroNome;
+	private String segundoNome;
+
+	public int getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(int idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getSegundoNome() {
+		return segundoNome;
+	}
+
+	public void setSegundoNome(String segundoNome) {
+		this.segundoNome = segundoNome;
+	}
 
 }
